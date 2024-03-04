2c15c65ac1b75733f29ff0e15f9865b00cadc7de
joannaberlin <joanna.kot.ok@gmail.com>
2024-03-04 11:04:25 +0100

    Change styling

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const btn = document.getElementById('get-activity');
 const activity = document.getElementById('activity');
+const title = document.getElementById('title');
+const body = document.body;
 
 btn.addEventListener('click', () => {
 	fetch('https://apis.scrimba.com/bored/api/activity')
 		.then((response) => response.json())
 		.then((data) => {
 			activity.textContent = data.activity;
+			title.textContent = '🦾 HappyBot🦿';
+			body.classList.add('fun');
 		});
 });
